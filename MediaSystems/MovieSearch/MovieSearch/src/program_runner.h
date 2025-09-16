bac93d77cf132dbcddbc0c7466e23bbc8c3a6495
Yme Brugts <yme@resolutionsbv.nl>
2025-09-16 21:41:53 +0200

    dataset added and setup main program

---
+++ MediaSystems/MovieSearch/MovieSearch/src/program_runner.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+* author Yme Brugts (s4536622)
+ * @file program_runner.h
+ * @date 2025-09-16
+ */
+
+
+#include <ostream>
+
+void RunProgram(std::istream& in, std::ostream& out, bool interactiveMode);
