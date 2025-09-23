1871d0a5b3ab6af0926af64204a16399592cebc7
Yme Brugts <yme@resolutionsbv.nl>
2025-09-24 01:08:19 +0200

    better naming

--- MediaSystems/MovieSearch/MovieParser/src/parsers/rating_parser.cpp
+++ MediaSystems/MovieSearch/MovieParser/src/parsers/rating_parser.cpp
@@ -12,32 +12,31 @@ namespace movie_parser::parsers
     std::vector<models::MovieRating> load_ratings(const std::string& filename, std::atomic<int>& progress) {
         std::vector<models::MovieRating> ratings;
 
-
-        const MappedFile mmap(filename);
-        if (mmap.empty()) {
+        const MappedFile mapped_file(filename);
+        if (mapped_file.empty()) {
             throw std::runtime_error("Could not open or map file: " + filename);
         }
 
-        const char* f = mmap.data();
-        const char* l = f + mmap.size();
+        const char* file_begin = mapped_file.data();
+        const char* file_end = file_begin + mapped_file.size();
 
         // Count total lines
         size_t total_lines = 0;
-        const char* ptr = f;
-        while (ptr && ptr != l) {
-            if ((ptr = static_cast<const char*>(memchr(ptr, '\n', l - ptr)))) {
+        const char* newline_search_ptr = file_begin;
+        while (newline_search_ptr && newline_search_ptr != file_end) {
+            if ((newline_search_ptr = static_cast<const char*>(memchr(newline_search_ptr, '\n', file_end - newline_search_ptr)))) {
                 ++total_lines;
-                ++ptr;
+                ++newline_search_ptr;
             }
         }
-    	ratings.reserve(total_lines);
+        ratings.reserve(total_lines);
 
         size_t current_line = 0;
-        const char* line_start = f;
-        for (const char* it = f; it != l; ++it) {
-            if (*it == '\n' || it == l - 1) {
-                const char* line_end = (*it == '\n') ? it : (it + 1);
-                std::string_view line(line_start, line_end - line_start);
+        const char* line_start_ptr = file_begin;
+        for (const char* char_ptr = file_begin; char_ptr != file_end; ++char_ptr) {
+            if (*char_ptr == '\n' || char_ptr == file_end - 1) {
+                const char* line_end_ptr = (*char_ptr == '\n') ? char_ptr : (char_ptr + 1);
+                std::string_view line(line_start_ptr, line_end_ptr - line_start_ptr);
 
                 size_t p1 = line.find("::");
                 size_t p2 = line.find("::", p1 + 2);
@@ -50,13 +49,15 @@ namespace movie_parser::parsers
                     models::MovieRating movie_rating;
                     std::from_chars(line.data(), line.data() + p1, movie_rating.user_id);
                     std::from_chars(line.data() + p1 + 2, line.data() + p2, movie_rating.movie_id);
-					// rating. We only need .5 precision, so we can parse manually
+                    // rating. We only need .5 precision, so we can parse manually
                     {
                         std::string_view rating_sv = line.substr(p2 + 2, p3 - (p2 + 2));
                         int whole = 0;
                         std::from_chars(rating_sv.data(), rating_sv.data() + rating_sv.size(), whole);
 
-                        if (rating_sv.size() > 2 && rating_sv[rating_sv.size() - 2] == '.' && rating_sv.back() == '5') {
+                        if (rating_sv.size() > 2 &&
+                            rating_sv[rating_sv.size() - 2] == '.' &&
+                            rating_sv.back() == '5') {
                             movie_rating.rating = whole + 0.5;
                         }
                         else {
@@ -73,7 +74,7 @@ namespace movie_parser::parsers
                         std::memory_order_relaxed);
                 }
 
-                line_start = it + 1; // next line starts after '\n'
+                line_start_ptr = char_ptr + 1; // next line starts after '\n'
             }
         }
 
@@ -81,4 +82,3 @@ namespace movie_parser::parsers
         return ratings;
     }
 }
-
