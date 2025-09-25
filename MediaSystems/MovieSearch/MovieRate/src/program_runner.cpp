04b0ca57a8c1ecaebb087ac52b39a101bf5a94c1
Yme Brugts <yme@resolutionsbv.nl>
2025-09-25 04:53:03 +0200

    small text fix

--- MediaSystems/MovieSearch/MovieRate/src/program_runner.cpp
+++ MediaSystems/MovieSearch/MovieRate/src/program_runner.cpp
@@ -98,11 +98,11 @@ void run_program(std::istream& in, std::ostream& out, bool interactive_mode) {
             else if (result.used_pearson) {
                 out << "The predicted rating for UserID: " << result.target_user_id
                     << " and MovieID: " << result.target_movie_id
-                    << " is " << result.predicted_rating
-                    << " (calculated using Pearson correlation scarcity fallback.\n\n"
-					"1. This takes into account all the ratings and users that have seen that same movie and calculate a correlation score based on the movies they both saw"
+                    << " is " << result.predicted_rating << "\n"
+                    << "Calculated using Pearson correlation fallback due to scarcity.\n\n"
+					"1. This takes into account all the ratings and users that have seen that same movie and calculate a correlation score based on the movies they both saw\n"
                     "2. Also calculate a weight based on the amount of movies that they have shared to give a weight to the correlation\n"
-					"3. Normalizes ratings by subtracting the user's mean. Meaning people can be inclined to rate higher or lower in general which should not effect correlation"
+					"3. Normalizes ratings by subtracting the user's mean. Meaning people can be inclined to rate higher or lower in general which should not affect correlation\n"
                     "--> Combine the correlation, weight and normalized rating to calculate the predicted score)\n";
             }
             else if (result.similar_user_id == result.target_user_id) {
